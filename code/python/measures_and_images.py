9912d4949c508c1f784bdea2d1e7a2f36651d3c2
Keith Cheveralls <keith.chev@gmail.com>
2023-10-31 15:40:09 -0700

    initial formatting

--- code/python/measures_and_images.py
+++ code/python/measures_and_images.py
@@ -1,59 +1,76 @@
 """
 Summary:
-The script is designed to process a single TIF image stack, compute its focus measure using various methods,
-and then save the computed images and measurements to specific directories. The focus measure methods available
-include variance of Laplacian, standard deviation of intensity (with and without Gaussian blur), and the Sobel method.
-The script also logs each step of the process. Results are saved both as images and as a CSV file containing focus measurements.
+The script is designed to process a single TIF image stack,
+compute its focus measure using various methods,
+and then save the computed images and measurements to specific directories.
+The focus measure methods available
+include variance of Laplacian, standard deviation of intensity
+(with and without Gaussian blur), and the Sobel method.
+The script also logs each step of the process.
+Results are saved both as images and as a CSV file containing focus measurements.
 """
+
+import csv  # Provides functions for reading from and writing to CSV files.
+import logging  # Provides functions for logging.
 
 # Import necessary libraries
 import os  # Provides functions for interacting with the operating system.
-import numpy as np  # Provides functions for numerical operations.
 import cv2  # OpenCV, a library for computer vision tasks.
+import numpy as np  # Provides functions for numerical operations.
+
 from skimage import io  # Provides functions for image reading and writing.
-import csv  # Provides functions for reading from and writing to CSV files.
-import logging  # Provides functions for logging.
-import time  # Provides various time-related functions.
 
 # Set up logging with specifications
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename='focus_measure.log')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='focus_measure.log',
+)
+
 
 # Define the variance of Laplacian focus measure method
 def variance_of_laplacian(frame):
     # Apply Gaussian blur to the frame
-    img = cv2.GaussianBlur(frame,(3,3),0)
+    img = cv2.GaussianBlur(frame, (3, 3), 0)
     # Compute the Laplacian of the blurred image
     laplacian = cv2.Laplacian(img, cv2.CV_64F)
     # Normalize the Laplacian image
-    laplacian_normalized = cv2.normalize(laplacian, None, 0, 65535, cv2.NORM_MINMAX, cv2.CV_16U)
+    laplacian_normalized = cv2.normalize(
+        laplacian, None, 0, 65535, cv2.NORM_MINMAX, cv2.CV_16U
+    )
     return laplacian_normalized, laplacian.var()
+
 
 # Define the Sobel focus measure method
 def sobel_method(frame):
     # Apply Gaussian blur to the frame
-    img = cv2.GaussianBlur(frame, (3,3), 0)
+    img = cv2.GaussianBlur(frame, (3, 3), 0)
     # Compute the x-gradient and y-gradient using Sobel operator
     sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
     sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
     # Compute the magnitude of the gradient
     magnitude = np.sqrt(sobelx**2 + sobely**2)
     # Normalize the magnitude image
-    magnitude_normalized = cv2.normalize(magnitude, None, 0, 65535, cv2.NORM_MINMAX, cv2.CV_16U)
+    magnitude_normalized = cv2.normalize(
+        magnitude, None, 0, 65535, cv2.NORM_MINMAX, cv2.CV_16U
+    )
     return magnitude_normalized, magnitude.var()
 
-# The Tenengrad method is another focus measure method, but it seems redundant with the Sobel method.
+
+# The Tenengrad method is another focus measure method,
+# but it seems redundant with the Sobel method.
 
 # Define the standard deviation of intensity focus measure method (without Gaussian blur)
 def std_dev_of_intensity(frame):
     return None, np.std(frame)  # This method does not generate an image, only a focus value
 
+
 # Define the standard deviation of intensity focus measure method (with Gaussian blur)
 def std_dev_of_intensity_with_gauss(frame):
     # Apply Gaussian blur to the frame
-    img = cv2.GaussianBlur(frame, (3,3), 0)
+    img = cv2.GaussianBlur(frame, (3, 3), 0)
     return None, np.std(img)  # This method does not generate an image, only a focus value
+
 
 # Function to compute focus measure based on the selected method
 def compute_focus_measure(frame, method):
@@ -69,6 +86,7 @@ def compute_focus_measure(frame, method):
     else:
         raise ValueError(f"Unknown focus measure method: {method}")
 
+
 # Save the computed image to a specified directory
 def save_computed_image(image, method, stack_id, frame_num):
     # Define the output directory based on method and stack_id
@@ -81,6 +99,7 @@ def save_computed_image(image, method, stack_id, frame_num):
     # Save the image to the specified path
     if image is not None:
         io.imsave(output_path, image.astype(np.uint16))
+
 
 # Function to process a single TIF stack and compute its focus measure using different methods
 def process_single_tif_stack(stack_path, all_data):
@@ -96,22 +115,32 @@ def process_single_tif_stack(stack_path, all_data):
 
     # For each focus measure method, compute the focus measure for each frame in the TIF stack
     for method in focus_methods:
-        computed_images_focus_values = [compute_focus_measure(frame, method) for frame in original_stack]
+        computed_images_focus_values = [
+            compute_focus_measure(frame, method) for frame in original_stack
+        ]
 
         for frame_num, (image, focus_value) in enumerate(computed_images_focus_values):
             logging.info(f"Processing frame {frame_num} for {stack_id} using {method}")
             save_computed_image(image, method, stack_id, frame_num)
-            all_data.append({
-                'stack_id': stack_id,
-                'method': method,
-                'frame': frame_num,
-                'focus_measure': focus_value
-            })
+            all_data.append(
+                {
+                    'stack_id': stack_id,
+                    'method': method,
+                    'frame': frame_num,
+                    'focus_measure': focus_value,
+                }
+            )
+
 
 # Entry point of the script
 if __name__ == "__main__":
     # List of focus measure methods to be used
-    focus_methods = ['variance_of_laplacian', 'std_dev_of_intensity', 'std_dev_of_intensity_with_gauss', 'sobel_method']
+    focus_methods = [
+        'variance_of_laplacian',
+        'std_dev_of_intensity',
+        'std_dev_of_intensity_with_gauss',
+        'sobel_method',
+    ]
     all_data = []
 
     # Process the TIF stack and compute its focus measure
