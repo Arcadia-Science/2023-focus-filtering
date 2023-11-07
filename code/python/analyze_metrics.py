60caf9e5386a6e89192be1a97a1769e1eebdaef0
Keith Cheveralls <keith.chev@gmail.com>
2023-11-06 17:23:39 -0800

    add subplot titles to make explicit which subplot rows correspond to brightfield and DIC

--- code/python/analyze_metrics.py
+++ code/python/analyze_metrics.py
@@ -83,19 +83,23 @@ def plot_all_roc_curves():
     and one column for each focus metric
     '''
 
+    imaging_modality_names = ('Brightfield', 'DIC')
+
     repo_dirpath = utils.find_repo_root(__file__)
     annotation_filepaths = list((repo_dirpath / 'analysis' / 'user_assessments').glob('*.csv'))
 
-    num_rows = len(('brightfield', 'dic'))
+    num_rows = len(imaging_modality_names)
     num_cols = len(calculate_metrics.ALL_FOCUS_METRICS)
-    _, axs = plt.subplots(num_rows, num_cols, figsize=(10, 5))
+    _, axs = plt.subplots(num_rows, num_cols, figsize=(12, 8))
 
     for filepath in annotation_filepaths:
         annotations_all = load_annotations_and_calc_metrics(filepath)
         annotations_bf, annotations_dic = split_annotations_by_modality(annotations_all)
 
         for col_ind, metric_name in enumerate(calculate_metrics.ALL_FOCUS_METRICS):
-            for row_ind, annotations in enumerate((annotations_bf, annotations_dic)):
+            for row_ind, (modality_name, annotations) in enumerate(
+                zip(imaging_modality_names, (annotations_bf, annotations_dic))
+            ):
                 ax = axs[row_ind][col_ind]
 
                 sorted_labels = annotations.sort_values(by=metric_name, ascending=True).InFocus
@@ -103,8 +107,10 @@ def plot_all_roc_curves():
                 roc_curve = calc_roc(sorted_labels)
                 ax.plot(roc_curve[0, :], roc_curve[1, :], color='#28b', alpha=0.5)
 
-                if row_ind == 0:
-                    ax.set_title(metric_name.replace('_', ' ').capitalize(), fontsize=10)
+                ax.set_title(
+                    '%s\n%s' % (metric_name.replace('_', ' ').capitalize(), modality_name),
+                    fontsize=10,
+                )
 
                 if row_ind == num_rows - 1:
                     ax.set_xlabel('False positive rate')
@@ -122,4 +128,4 @@ if __name__ == '__main__':
     plot_all_roc_curves()
     plot_dirpath = utils.find_repo_root(__file__) / 'output'
     os.makedirs(plot_dirpath, exist_ok=True)
-    plt.savefig(plot_dirpath / 'roc_curves.svg', format='svg')
+    plt.savefig(plot_dirpath / 'focus_metric_roc_curves.svg', format='svg')
