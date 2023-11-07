9be9cef6cf57aba54f44f84bfe4c94805c3800f4
Keith Cheveralls <keith.chev@gmail.com>
2023-11-07 13:22:37 -0800

    add calculation of the median true-positive rate for a given false-positive rate

--- code/python/analyze_metrics.py
+++ code/python/analyze_metrics.py
@@ -7,6 +7,8 @@ import utils
 
 from matplotlib import pyplot as plt
 
+IMAGING_MODALITY_NAMES = ('Brightfield', 'DIC')
+
 
 def calc_roc(sorted_labels):
     '''
@@ -57,10 +59,9 @@ def load_annotations_and_calc_metrics(filepath):
     return annotations
 
 
-def split_annotations_by_modality(annotations):
+def filter_annotations_by_modality(annotations, modality_name):
     '''
-    split an annotations dataframe into two dataframes,
-    one for the brightfield ('bf') images and one for the DIC images in the original stack
+    filter an annotations dataframe to include only the rows from a given imaging modality
 
     WARNING: this is specific to the stack and annotations included in this repo
     (`experiment_images/sampled_sequence.tif` and `analysis/user_assessments/` respectively)
@@ -69,10 +70,14 @@ def split_annotations_by_modality(annotations):
     # at which the frames switch from brightfield ('bf') to DIC
     bf_dic_ind = 90
 
-    annotations_bf = annotations.iloc[:bf_dic_ind].copy().reset_index()
-    annotations_dic = annotations.iloc[bf_dic_ind:].copy().reset_index()
+    if modality_name.lower() == 'brightfield':
+        annotations = annotations.iloc[:bf_dic_ind].copy().reset_index()
+    elif modality_name.lower() == 'dic':
+        annotations = annotations.iloc[bf_dic_ind:].copy().reset_index()
+    else:
+        raise ValueError(f"Unknown modality: {modality_name}")
 
-    return annotations_bf, annotations_dic
+    return annotations
 
 
 def plot_all_roc_curves():
@@ -83,25 +88,21 @@ def plot_all_roc_curves():
     and one column for each focus metric
     '''
 
-    imaging_modality_names = ('Brightfield', 'DIC')
-
     repo_dirpath = utils.find_repo_root(__file__)
     annotation_filepaths = list((repo_dirpath / 'analysis' / 'user_assessments').glob('*.csv'))
 
-    num_rows = len(imaging_modality_names)
+    num_rows = len(IMAGING_MODALITY_NAMES)
     num_cols = len(calculate_metrics.ALL_FOCUS_METRICS)
     _, axs = plt.subplots(num_rows, num_cols, figsize=(12, 8))
 
     for filepath in annotation_filepaths:
         annotations_all = load_annotations_and_calc_metrics(filepath)
-        annotations_bf, annotations_dic = split_annotations_by_modality(annotations_all)
 
         for col_ind, metric_name in enumerate(calculate_metrics.ALL_FOCUS_METRICS):
-            for row_ind, (modality_name, annotations) in enumerate(
-                zip(imaging_modality_names, (annotations_bf, annotations_dic))
-            ):
+            for row_ind, modality_name in enumerate(IMAGING_MODALITY_NAMES):
                 ax = axs[row_ind][col_ind]
 
+                annotations = filter_annotations_by_modality(annotations_all, modality_name)
                 sorted_labels = annotations.sort_values(by=metric_name, ascending=True).InFocus
 
                 roc_curve = calc_roc(sorted_labels)
@@ -124,8 +125,48 @@ def plot_all_roc_curves():
                 ax.set_aspect(1)
 
 
+def calc_median_tpr(fpr_thresh):
+    '''
+    Calculate the median true positive rate across all imaging modalities
+    for a given false positive rate threshold
+    '''
+
+    repo_dirpath = utils.find_repo_root(__file__)
+    annotation_filepaths = list((repo_dirpath / 'analysis' / 'user_assessments').glob('*.csv'))
+
+    summary_rows = []
+    for filepath in annotation_filepaths:
+        annotations_all = load_annotations_and_calc_metrics(filepath)
+
+        for metric_name in calculate_metrics.ALL_FOCUS_METRICS:
+            for modality_name in IMAGING_MODALITY_NAMES:
+                annotations = filter_annotations_by_modality(annotations_all, modality_name)
+                sorted_labels = annotations.sort_values(by=metric_name, ascending=True).InFocus
+                roc_curve = calc_roc(sorted_labels)
+
+                # calculate the true positive rate at the FPR closest
+                # to a given false positive rate
+                ind = np.argmin((roc_curve[0, :] - fpr_thresh) ** 2)
+                summary_rows.append(
+                    {
+                        'modality': modality_name,
+                        'metric': metric_name,
+                        'fpr': roc_curve[0, ind],
+                        'tpr': roc_curve[1, ind],
+                    }
+                )
+
+    summary = pd.DataFrame(summary_rows)
+    summary = summary.groupby(['modality', 'metric']).median().reset_index()
+    return summary
+
+
 if __name__ == '__main__':
+    output_dirpath = utils.find_repo_root(__file__) / 'analysis' / 'figures'
+    os.makedirs(output_dirpath, exist_ok=True)
+
     plot_all_roc_curves()
-    plot_dirpath = utils.find_repo_root(__file__) / 'analysis' / 'figures'
-    os.makedirs(plot_dirpath, exist_ok=True)
-    plt.savefig(plot_dirpath / 'focus_metric_roc_curves.svg', format='svg')
+    plt.savefig(output_dirpath / 'focus_metric_roc_curves.svg', format='svg')
+
+    summary = calc_median_tpr(fpr_thresh=0.05)
+    summary.to_csv(output_dirpath / 'roc_curve_summary.csv', index=False, float_format='%.3f')
